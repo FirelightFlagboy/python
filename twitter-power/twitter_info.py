c2614a9cd09854829a32ad832d60ffd21a385874
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-10-10 20:32:04 +0200

    add twitter power

---
+++ twitter-power/twitter_info.py
@@ -0,0 +1,5 @@
+OAUTH_TOKEN = "Access Token"
+OAUTH_SECRET = "Access Token Secret"
+CONSUMER_KEY = "Consumer Key (API Key)"
+CONSUMER_SECRET = "Consumer Secret (API Secret)"
+TWITTER_HANDLE = "Pseudo Twitter sans @"
