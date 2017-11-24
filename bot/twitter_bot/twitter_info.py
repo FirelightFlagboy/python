a4bab1071db9cf36248c048bb601fa40b06a2e54
FirelightFlagboy <firelight.flagboy@gmail.com>
2017-11-24 21:28:13 +0100

    mv twitter bot to bot folder

---
+++ bot/twitter_bot/twitter_info.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+app_key = "58CrdRFLNcJGdrvR8Ogz6uMES"
+app_secret = "nWmWdxza8W8QI93HXGHAnY40sAmwq1W3yeNZlBLA96a6nscvNG"
+oauth_token = "2811632197-RRyXIZO88o6ZO1rKZccdjDuBdhOJJU6q7d9lxKQ"
+oauth_token_secret = "QrCtxkTnRnSg9i7z4j4fzNMXpKCg1pWKw8NSZ99IXPiy0"
+
+def ft_get_appkey():
+	return app_key
+
+def ft_get_appsecret():
+	return app_secret
+
+def ft_get_oauthtoken():
+	return oauth_token
+
+def ft_get_oauthsecret():
+	return oauth_token_secret
