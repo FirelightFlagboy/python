d4f9f57ae1662f0b582e3e673cda4fb6d60a635d
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-04-14 21:51:03 +0200

    work on mooc

---
+++ mooc/system/argparse_test.py
@@ -0,0 +1,17 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("x", help="le nombre a mettre au carré", type=int)
+parser.add_argument("-v", "--verbose", action="store_true",
+	help="augmente la verbosité")
+
+arg = parser.parse_args()
+print("le nm a mettre au carre est =", arg.x)
+x = arg.x
+sqr_x = x ** 2
+
+if arg.verbose:
+	print("{} ^ 2 = {}".format(x, sqr_x))
+else:
+	print(">>", sqr_x)
